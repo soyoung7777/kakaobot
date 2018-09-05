27b74d476274e5a5ffa1370af9e7c4ca1daf5048
kimhyemin <>
2018-09-05 17:41:29 +0900

    no message

--- Django/chat/models.py
+++ Django/chat/models.py
@@ -2,12 +2,12 @@ from django.db import models
 from jsonfield import JSONField
 
 class allData(models.Model):
-	user = models.CharField(max_length=128, primary_key=True)
+	session_id = models.CharField(max_length=128, primary_key=True)
 	jsondata = models.TextField()
 	dialogflow_action = models.IntegerField(default=0)
 	bus_action = models.IntegerField(default=0)
     bus_station_result = models.IntegerField(default=0)
-    bus_selected = models.IntegerField(default=0)
+    bus_selected = models.CharField(max_length=128)
     bus_arsid = models.IntegerField(default=0)
 
 
